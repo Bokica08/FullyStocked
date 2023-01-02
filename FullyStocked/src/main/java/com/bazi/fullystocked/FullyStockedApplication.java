9f78098536218672ba58af188d8b0eb33c118717
Bojan <bojantrpeski123@gmail.com>
2023-01-02 12:26:53 +0100

    Initial commit

---
+++ FullyStocked/src/main/java/com/bazi/fullystocked/FullyStockedApplication.java
@@ -0,0 +1,13 @@
+package com.bazi.fullystocked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullyStockedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullyStockedApplication.class, args);
+    }
+
+}
---
+++ FullyStocked/src/test/java/com/bazi/fullystocked/FullyStockedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bazi.fullystocked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullyStockedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
