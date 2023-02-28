333c40d0bae54c4efa1782e9a6308318135012bf
DarkoSasanski <darko.sasanski@gmail.com>
2023-02-28 13:42:50 +0100

    Minor fixes

--- FullyStocked/src/main/java/com/bazi/fullystocked/Services/Implementations/WorkersServiceImpl.java
+++ FullyStocked/src/main/java/com/bazi/fullystocked/Services/Implementations/WorkersServiceImpl.java
@@ -45,34 +45,34 @@ public class WorkersServiceImpl implements WorkersService {
     @Override
     public List<TopUsersDTO> findAllTopUsers() {
         List<TopUsersDTO> results = entityManager.createNativeQuery("""
-        select u.firstname, u.lastname, u.username, u.email, coalesce(max(ti.totalPrice), 0) as topInvoiceSum,
-       (select a.articlename from project.articles a
-           left join project.invoicedarticles i2 on i2.articleid=a.articleid
-           where i2.price*i2.quantity=max(ba.totalPrice)
-           limit 1) as topArticleName,
-       (select i2.price  from project.articles a
-           left join project.invoicedarticles i2 on i2.articleid=a.articleid
-           where i2.price*i2.quantity=max(ba.totalPrice)
-           limit 1) as topArticlePrice,
-       (select i2.quantity from project.articles a
-           left join project.invoicedarticles i2 on i2.articleid=a.articleid
-           where i2.price*i2.quantity=max(ba.totalPrice)
-           limit 1) as topArticleQuantity,
-       max(ba.totalPrice) as topArticleTotalPrice from project.workers w
-               left join project.users u on u.userid=w.userid
-               left join
-                   (
-                       select i.invoiceid, i.workeruserid , sum(ia.price*ia.quantity) as totalPrice from project.invoices i
-                       left join project.invoicedarticles ia on ia.invoiceid=i.invoiceid
-                       group by i.invoiceid, i.workeruserid
-                   ) ti on ti.workeruserid=u.userid
-               left join
-                   (
-                       select ia2.invoiceid, a.articlename, ia2.price as price, ia2.quantity as quantity, ia2.price*ia2.quantity as totalPrice from project.invoicedarticles ia2
-                       left join project.articles a on a.articleid=ia2.articleid
-                   ) ba on ba.invoiceid=ti.invoiceid
-               group by u.userid
-               order by topInvoiceSum desc
+ select u.firstname, u.lastname, u.username, u.email, coalesce(max(ti.totalPrice), 0) as topInvoiceSum,
+  (select a.articlename from project.articles a
+      left join project.invoicedarticles i2 on i2.articleid=a.articleid
+      where i2.price*i2.quantity=max(ba.totalPrice) and i2.invoiceid = min(ti.invoiceid)
+      ) as topArticleName,
+  (select i2.price  from project.articles a
+      left join project.invoicedarticles i2 on i2.articleid=a.articleid
+      where i2.price*i2.quantity=max(ba.totalPrice) and i2.invoiceid = min(ti.invoiceid)
+      ) as topArticlePrice,
+  (select i2.quantity from project.articles a
+      left join project.invoicedarticles i2 on i2.articleid=a.articleid
+      where i2.price*i2.quantity=max(ba.totalPrice) and i2.invoiceid = min(ti.invoiceid)
+      ) as topArticleQuantity,
+  max(ba.totalPrice) as topArticleTotalPrice from project.workers w
+          left join project.users u on u.userid=w.userid
+          left join
+              (
+                  select i.invoiceid, i.workeruserid , sum(ia.price*ia.quantity) as totalPrice from project.invoices i
+                  left join project.invoicedarticles ia on ia.invoiceid=i.invoiceid
+                  group by i.invoiceid, i.workeruserid
+              ) ti on ti.workeruserid=u.userid
+          left join
+              (
+                  select ia2.invoiceid, a.articlename, ia2.price as price, ia2.quantity as quantity, ia2.price*ia2.quantity as totalPrice from project.invoicedarticles ia2
+                  left join project.articles a on a.articleid=ia2.articleid
+              ) ba on ba.invoiceid=ti.invoiceid
+          group by u.userid
+          order by topInvoiceSum desc
 """)
                 .unwrap(NativeQuery.class)
                 .setResultTransformer(Transformers.aliasToBean(TopUsersDTO.class))
